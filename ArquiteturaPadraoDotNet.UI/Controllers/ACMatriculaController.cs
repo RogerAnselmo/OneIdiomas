99bcf77f8f3f633717f2cad97e84262fa6214c7c
rogeranselmo <rogerio_anselmo@hotmail.com>
2019-05-17 00:40:35 -0300

    crud matricula - iniciando crud matrícula

---
+++ ArquiteturaPadraoDotNet.UI/Controllers/ACMatriculaController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using One.Infra.CrossCutting.Identity.Data.Models;
+using One.UI.Helpers;
+
+namespace One.UI.Controllers
+{
+    [Route("Gerenciar-Matricula")]
+    public class ACMatriculaController : BaseController
+    {
+        public ACMatriculaController(IOptions<BaseUrl> baseUrl, UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager) : base(baseUrl, userManager, signInManager)
+        {
+        }
+
+        [Route("Cadastro-Matricula")]
+        public IActionResult Cadastro()
+        {
+            ViewBag.BaseUrl = ObterBaseUrl();
+            ViewBag.Title = "Cadastro de Matrículas";
+            return View();
+        }
+
+        [Route("Selecionar-Aluno")]
+        public IActionResult SelecionarAluno()
+        {
+            return View();
+        }
+    }
+}
